faee3e2a1c1beef5f02b7decb482ac56963a3d4a
Turek69420 <150358928+Turek69420@users.noreply.github.com>
2023-11-09 11:53:05 -0300

    Boitei os arquivos:style.css, index.html, sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("crimson");
+  }
+  
+  function draw() {
+    fill("gold");
+    stroke("gold");
+    if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 20);
+    
+    
+  }
+  }
